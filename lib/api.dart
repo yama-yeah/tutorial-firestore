cdbdae9ce0e6f60662591ab48defce17d96a341a
unkonown <b1021091@fun.ac.jp>
2021-12-07 04:17:00 +0900

    [add] Api class

--- lib/api.dart
+++ lib/api.dart
@@ -1,3 +1,26 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+
+class Api {
+  String BASE_URL = "https://fun-manaba-api.azurewebsites.net/";
+  Map<String, String> user = {'userid': '', 'password': ''};
+  Map<String, String> headers = {'content-type': 'application/json'};
+  void User(String userid, String password) {
+    user['userid'] = userid;
+    user['password'] = password;
+  }
+
+  Future<String> auth() async {
+    String body = json.encode(user);
+    String url = BASE_URL + "auth";
+    var raw =
+        await http.post(Uri.parse(url + 'login'), body: body, headers: headers);
+    return json2map(raw)['token'];
+  }
+
+  Map<String, dynamic> json2map(http.Response json) {
+    String str = utf8.decode(json.bodyBytes);
+    return jsonDecode(str);
+  }
+}
