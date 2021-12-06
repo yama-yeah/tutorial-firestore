850f60e73800e13368aacac0d4d9b60ce34a3d1b
unkonown <b1021091@fun.ac.jp>
2021-12-07 00:50:44 +0900

    firest commit

---
+++ lib/post.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class PostPage extends StatefulWidget {
+  @override
+  _PostPagePageState createState() => _PostPagePageState();
+}
+
+class _PostPagePageState extends State<PostPage> {
+  TextEditingController _textEditingController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("投稿画面"),
+      ),
+      body: Center(
+        child: TextField(
+          controller: _textEditingController,
+          enabled: true,
+          maxLength: 50, // 入力数
+          maxLengthEnforced: false, // 入力上限になったときに、文字入力を抑制するか
+          style: TextStyle(color: Colors.black),
+          obscureText: false,
+          maxLines: 1,
+          decoration: const InputDecoration(
+            icon: Icon(Icons.speaker_notes),
+            hintText: '投稿内容を記載します',
+            labelText: '内容 * ',
+          ),
+        ),
+      ),
+    );
+  }
+}
