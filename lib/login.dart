0be70dc60044a8a00342d7a5fd5de5de955c840f
unkonown <b1021091@fun.ac.jp>
2021-12-07 03:52:02 +0900

    strat develop

---
+++ lib/login.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(children: [
+          TextField(
+            decoration: InputDecoration(
+              hintText: 'Enter your userid',
+            ),
+          ),
+          TextField(
+            decoration: InputDecoration(
+              hintText: 'Enter your password',
+            ),
+          ),
+          RaisedButton(
+            child: Text('Login'),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => HomePage()),
+              );
+            },
+          ),
+        ]),
+      ),
+    );
+  }
+}
